f8ab4740726bedff0b5d5b452acd3f4be1a31fca
Brooke Fisher <brookenfish1@gmail.com>
2019-03-31 14:20:10 -0400

    Create basic tree structure through JavaScript

--- vehicle-picker/utils/data-tree-transform.js
+++ vehicle-picker/utils/data-tree-transform.js
@@ -3,7 +3,31 @@ const fetch = require("node-fetch");
 const dataURL = "https://gist.githubusercontent.com/leetrout/06691832f46d91ea0d533b5280467491/raw/cca39786bcddd93a4aebe88ade03b5c9f33b1fb0/vehicle_flat.json";
 
 function makeTree(fullData) {
+  const byYear = {};
 
+  fullData.forEach((r) => {
+    // console.log("The current record is", r);
+    const strYear = String(r.year);
+    let yearData = byYear[strYear];
+
+    if (yearData === undefined) {
+      yearData = {};
+    }
+
+    let makeData = yearData[r.make];
+
+    if (makeData === undefined) {
+      makeData = {};
+    }
+
+    makeData[r.model] = r.id;
+    yearData[r.make] = makeData;
+
+    byYear[strYear] = yearData;
+  });
+
+  console.log(byYear);
+  return byYear;
 }
 
 function main() {
