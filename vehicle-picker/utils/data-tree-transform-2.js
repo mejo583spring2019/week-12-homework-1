bcd02eb0009e76e54fc8724d639bf05fdae00003
Brooke Fisher <brookenfish1@gmail.com>
2019-03-31 16:50:13 -0400

    Create byMake.json file to sort data in make-year-model structure

--- vehicle-picker/utils/data-tree-transform-2.js
+++ vehicle-picker/utils/data-tree-transform-2.js
@@ -3,14 +3,32 @@ const fs = require("fs");
 
 const dataURL = "https://gist.githubusercontent.com/leetrout/06691832f46d91ea0d533b5280467491/raw/cca39786bcddd93a4aebe88ade03b5c9f33b1fb0/vehicle_flat.json";
 
-function makeMakeYearModelTree(fullData) {
+function makeTree(fullData) {
   const byMake = {};
+
+  fullData.forEach((r) => {
+    const strYear = String(r.year);
+
+    let makeData = byMake[r.make];
+    if (makeData === undefined) {
+      makeData = {};
+    }
+
+    let yearData = makeData[strYear];
+    if (yearData === undefined) {
+      yearData = {};
+    }
+
+    yearData[r.model] = r.id;
+    makeData[strYear] = yearData;
+    byMake[r.make] = makeData;
+  });
 
   return byMake;
 }
 
 function writeJSONFile(filename, fullData) {
-  const data = makeMakeYearModelTree(fullData);
+  const data = makeTree(fullData);
   const jsonData = JSON.stringify(data);
   fs.writeFileSync(filename, jsonData, "utf8");
 }
