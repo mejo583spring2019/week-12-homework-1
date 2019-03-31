0a81bffedb494dedb4d119e3941ff8ea29b1bf4b
Brooke Fisher <brookenfish1@gmail.com>
2019-03-31 14:59:08 -0400

    Add years to the first select box

--- vehicle-picker/src/App.js
+++ vehicle-picker/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yearMakeModel: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch("/byYear.json")
+        .then((res) => res.json())
+        .then((json) => (this.setState({ yearMakeModel: json })));
+  }
+
+  getYearOptions() {
+    return Object.keys(this.state.yearMakeModel).map((k) =>
+      <option>{k}</option>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <select >
+            {this.getYearOptions()}
+          </select>
+
+          <select >
+            <option>World</option>
+          </select>
+
+          <select >
+            <option>!</option>
+          </select>
+        </div>
       </div>
     );
   }
