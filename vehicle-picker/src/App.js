8abad044830eff10c7843bec81107126ec62bc58
“SivonneWard” <sivonne@live.unc.edu>
2019-03-31 17:41:12 -0400

    added year selector

--- vehicle-picker/src/App.js
+++ vehicle-picker/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yearMakeModel: {},
+    };
+  }
+  componentDidMount() {
+    fetch("/byyear.json")
+        .then((res) => res.json())
+        .then((json) => this.setState({ yearMakeModel: json }));
+  }
+
+  getYearOptions() {
+    return Object.keys(this.state.yearMakeModel).map(
+        (k) => <option>{k}</option>
+    );
+    // we use map since it's going to return something (forEach doesn't)
+  }
+  handleYearSelect(evt) {
+    console.log("year selection changed", evt);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <select onChange={this.handleYearSelect.bind(this)}>
+            {this.getYearOptions()}
+          </select>
+          <select>
+            <option>World</option>
+          </select>
+          <select>
+            <option>!</option>
+          </select>
+
+        </div>
       </div>
     );
   }
