6b06be85b6abeb203834a50d8eb63e7268bcee93
“SivonneWard” <sivonne@live.unc.edu>
2019-03-31 16:56:54 -0400

    Add vehicle data app

---
+++ vehicle-picker/utils/practiceMakingTrees.js
@@ -0,0 +1,15 @@
+// // flat records
+// [
+//   { "id": 1, "year": 1909, "make": "Ford", "model": "Model T" },
+//   { "id": 2, "year": 1909, "make": "Chrysler", "model": "Imperial" },
+//   { "id": 3, "year": 1909, "make": "Chrysler", "model": "2CV" },
+// ];
+// // tree/hierarchical records
+// const byYearMakeModel = {
+//   1909: {
+// 1909 is a key, becomes a string, must use array access notation to call []
+//     Ford: ["Model T"],
+//     Chrysler: ["Imperial", "2CV"],
+//   },
+
+// };
