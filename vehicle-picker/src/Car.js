159c7c7b3af3072363fd4997860a7e9fe093f282
Brooke Fisher <brookenfish1@gmail.com>
2019-03-31 20:57:56 -0400

    Add section that reads how many years data is available for

--- vehicle-picker/src/Car.js
+++ vehicle-picker/src/Car.js
@@ -6,10 +6,7 @@ class Car extends Component {
     super(props);
     this.state = {
       makeYearModel: {},
-      yearSelected: "",
       makeSelected: "",
-      modelSelected: "",
-      modelName: "",
       yearsOperated: "",
       yearsInJSON: "",
     };
@@ -25,30 +22,6 @@ class Car extends Component {
     return Object.keys(this.state.makeYearModel).map((k) =>
       <option key={k} value={k}>{k}</option>
     );
-  }
-
-  getYearOptions() {
-    if (this.state.makeSelected) {
-      const make = this.state.makeSelected;
-      const makeData = this.state.makeYearModel[make];
-      return Object.keys(makeData).map((k) =>
-        <option key={k + make} value={k}>{k}</option>
-      );
-    }
-    return [];
-  }
-
-  getModelOptions() {
-    if (this.state.yearSelected !== "" && this.state.makeSelected !== "") {
-      const year = this.state.yearSelected;
-      const make = this.state.makeSelected;
-      const makeData = this.state.makeYearModel[make];
-      const yearData = makeData[year];
-      return Object.keys(yearData).map(
-          (k) => <option key={yearData[k]} value={yearData[k]}>{k}</option>
-      );
-    }
-    return [];
   }
 
   getYearLength() {
@@ -77,26 +50,7 @@ class Car extends Component {
 
   handleMakeSelect(evt) {
     this.setState({
-      yearSelected: "",
       makeSelected: evt.target.value,
-      modelSelected: "",
-      modelName: "",
-    });
-  }
-
-  handleYearSelect(evt) {
-    this.setState({
-      yearSelected: evt.target.value,
-      modelSelected: "",
-      modelName: "",
-    });
-  }
-
-
-  handleModelSelect(evt) {
-    this.setState({
-      modelSelected: evt.target.value,
-      modelName: evt.target[evt.target.selectedIndex].text,
     });
   }
 
@@ -120,42 +74,32 @@ class Car extends Component {
         <select onChange={this.handleMakeSelect.bind(this)}>
           <option value="">Make</option>
           {this.getMakeOptions()}
-        </select> &nbsp; to see how long they have been
-                      in business and what types of cars they offer.
+        </select> &nbsp; to see how long they have been in business.
         </h2>
         <h3>{this.state.makeSelected} has been around for
         &nbsp;<span
             value="{this.yearsOperated}"
             onChange="{this.handleYearLength.bind(this)}"
           >
-            {this.getYearLength()}</span>&nbsp; years.
-            We have data for <span onChange="{this.handleJsonYearData.bind(this)}">
+            {this.getYearLength()}</span>&nbsp; years. We have data for&nbsp;
+          <span
+            onChange="{this.handleJsonYearData.bind(this)}"
+          >
             {this.getNoOfYearsinJSON()}
-          </span>&nbsp; years
+          </span>&nbsp;years
         </h3>
-
-        <select
-          disabled={!this.state.makeSelected}
-          onChange={this.handleYearSelect.bind(this)}
-        >
-          <option value="">Year</option>
-          {this.getYearOptions()}
-        </select>
-
-        <select
-          disabled={!this.state.yearSelected || !this.state.makeSelected}
-          onChange={this.handleModelSelect.bind(this)}
-        >
-          <option value="">Model</option>
-          {this.getModelOptions()}
-        </select>
         <p>Reason for doing a selector this way: When I created my second tree,
           I grouped it from Make, Year, and Model.
           So, when you play this "Mad Libs", you'll have some information that
           dynamically gets loaded into it
           (such as # of years, # of models, etc)
+        </p>
+        <p>
           Additonally, the information may be inaccurate because some
           car manufacturers have went out of business.
+          If we only have data for a car manufacturer
+          for a small number of  years,
+          then it could be assumed the manufacturer is no longer in business.
         </p>
       </div>
     );
--- vehicle-picker/utils/data-tree-transform-2.js
+++ vehicle-picker/utils/data-tree-transform-2.js
@@ -1,5 +1,6 @@
-const fetch = require("node-fetch");
 const fs = require("fs");
+const fetch = require("node-fetch");
+
 
 const dataURL = "https://gist.githubusercontent.com/leetrout/06691832f46d91ea0d533b5280467491/raw/cca39786bcddd93a4aebe88ade03b5c9f33b1fb0/vehicle_flat.json";
 
