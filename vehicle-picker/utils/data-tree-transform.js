93de2d8b6e3d50fb7570b13078f972cf5aa15689
Taylor Tyson1 <ttay921@live.unc.edu>
2019-04-02 00:36:58 -0400

    do the hw

--- vehicle-picker/utils/data-tree-transform.js
+++ vehicle-picker/utils/data-tree-transform.js
@@ -1,14 +1,26 @@
 
 const fetch = require("node-fetch");
 
-const dataURL = "https://gist.githubusercontent.com/leetrout/06691832f46d91ea0d533b5280467491/raw/cca39786bcddd93a4aebe88ade03b5c9f33b1fb0/vehicle_flat.json"
-    ;
+const dataURL = "https://gist.githubusercontent.com/leetrout/06691832f46d91ea0d533b5280467491/raw/cca39786bcddd93a4aebe88ade03b5c9f33b1fb0/vehicle_flat.json";
+
+
+function makeTree(fullData) {
+  const byYear = {};
+
+  fullData.forEach((r) => {
+    console.log("The current record is", r);
+  });
+
+  return byYear;
+}
+
+
 function main() {
   console.log(dataURL);
 
   fetch(dataURL)
       .then((res) => res.json())
-      .then((json) => console.log(json.length));
+      .then((json) => makeTree(json));
 }
 
 main();
