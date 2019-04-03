dfcd2a304f351745947f529bd4dac35a7cc008c2
Taylor Tyson1 <ttay921@live.unc.edu>
2019-04-02 23:27:14 -0400

    make updated

--- vehicle-picker/src/App.js
+++ vehicle-picker/src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
     this.state = {
       yearMakeModel: {},
       yearSelected: "",
+      makeSelected: "",
+      modelSelected: "",
     };
   }
 
@@ -18,12 +20,41 @@ class App extends Component {
 
   getYearOptions() {
     return Object.keys(this.state.yearMakeModel).map((k) =>
-      <option key={k} value={k}>{k}</option>);
+      <option key={k} value={k}>{k}</option>
+    );
+  }
+
+  getMakeOptions() {
+    if (this.state.yearSelected) {
+      const yearData = this.state.yearMakeModel[this.state.yearSelected];
+      return Object.keys(yearData).map(
+          (k) => <option key={k} value={k}>{k}</option>);
+    }
+    return [];
+  }
+
+  getModelOptions() {
+    if (this.state.makeSelected) {
+      const makeData = this.state.yearMakeModel[this.state.makeSelected];
+      return Object.keys(makeData).map(
+          (k) => <option key={k} value={k}>{k}</option>);
+    }
+    return [];
   }
 
   handleYearSelect(evt) {
     this.setState({
       yearSelected: evt.target.value,
+    });
+  }
+  handleMakeSelect(evt) {
+    this.setState({
+      makeSelected: evt.target.value,
+    });
+  }
+  handleModelSelect(evt) {
+    this.setState({
+      modelSelected: evt.target.value,
     });
   }
 
@@ -35,11 +66,21 @@ class App extends Component {
             <option value="">Year</option>
             {this.getYearOptions()}
           </select>
-          <select disabled={!this.state.yearSelected}>
+
+
+          <select
+            disabled={!this.state.yearSelected}
+            onChange={this.handleMakeSelect.bind(this)}>
             <option>Make</option>
+            {this.getMakeOptions()}
           </select>
-          <select disabled={!this.state.yearSelected}>
+
+          <select
+            disabled={!this.state.yearSelected}
+            onChange={this.handleModelSelect.bind(this)}
+          >
             <option>Model</option>
+            {this.getModelOptions()}
           </select>
         </div>
       </div>
