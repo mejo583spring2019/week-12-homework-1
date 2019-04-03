f42838b542a99e086b177bcfc5295dd1195c1d61
Andrea Brucculeri <andrea@brucculeri.com>
2019-04-03 17:29:09 -0400

    add vehicle data app

---
+++ vehicle-picker/utils/datatree.js
@@ -0,0 +1,44 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const dataURL = "https://gist.githubusercontent.com/leetrout/06691832f46d91ea0d533b5280467491/raw/cca39786bcddd93a4aebe88ade03b5c9f33b1fb0/vehicle_flat.json"
+;
+
+function makeTree(fullData) {
+  const byYear = {};
+  fullData.forEach((r) => {
+    const strYear = String(r.year);
+    let yearData = byYear[strYear];
+    if (yearData === undefined) {
+      yearData = {};
+    }
+
+    let makeData = yearData[r.make];
+    if (makeData === undefined) {
+      makeData = {};
+    }
+
+    makeData[r.model] = r.id;
+    yearData[r.make] = makeData;
+
+    byYear[strYear] = yearData;
+  });
+  return byYear;
+}
+
+function writeJSONFile(filename, fullData) {
+  const data = makeTree(fullData);
+  const jsonData = JSON.stringify(data);
+  fs.writeFileSync(filename, jsonData, "utf8");
+}
+
+function main() {
+  process.stdout.write(`Loading data from ${dataURL}\n`);
+  const filename = "public/byyear.json";
+  fetch(dataURL)
+      .then((res) => res.json())
+      .then((json) => writeJSONFile(filename, json));
+
+  process.stdout.write(`Data written to ${filename}\n`);
+}
+
+main();
