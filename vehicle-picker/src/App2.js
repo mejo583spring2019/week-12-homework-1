3683f0fb6e35e2b3cd62e2145a8082e00897d43a
Austin Phillips <cochapel@live.unc.edu>
2019-04-06 16:10:13 -0400

    Final Commit 2

--- vehicle-picker/src/App2.js
+++ vehicle-picker/src/App2.js
@@ -6,6 +6,10 @@ class App2 extends Component {
     super(props);
     this.state = {
       makeModelYear: {},
+      makeSelected: "",
+      modelSelected: "",
+      yearSelected: "",
+      modelIndex: "",
     };
   }
 
@@ -17,33 +21,84 @@ class App2 extends Component {
 
   getMakeOptions() {
     return Object.keys(this.state.makeModelYear).map((k) =>
-      <option>{k}</option>);
+      <option key={k} value={k}>{k}</option>);
   }
 
   getModelOptions() {
-    return Object.keys(this.state.makeModelYear).map((k) =>
-      <option>{k}</option>);
+    if (this.state.makeSelected) {
+      const make = this.state.makeSelected;
+      const makeData = this.state.makeModelYear[make];
+      return Object.keys(makeData).map((k) =>
+        <option key={k + make} value={k}>{k}</option>);
+    }
+    return [];
   }
 
   getYearOptions() {
-    return Object.keys(this.state.makeModelYear).map((k) =>
-      <option>{k}</option>);
+    if (this.state.modelSelected !== "" && this.state.makeSelected !== "") {
+      const make = this.state.makeSelected;
+      const model = this.state.modelSelected;
+      const makeData = this.state.makeModelYear[make];
+      const modelData = makeData[model];
+      return Object.keys(modelData).map((k) =>
+        <option key={modelData[k]} value={modelData[k]}>{k}</option>);
+    }
+    return [];
+  }
+
+  handleMakeSelect(evt) {
+    this.setState({
+      makeSelected: evt.target.value,
+      modelSelected: "",
+      yearSelected: "",
+      modelIndex: "",
+    });
+  }
+
+  handleModelSelect(evt) {
+    this.setState({
+      modelSelected: evt.target.value,
+      yearSelected: "",
+      modelIndex: "",
+    });
+  }
+
+  handleYearSelect(evt) {
+    this.setState({
+      yearSelected: evt.target[evt.target.selectedIndex].text,
+      modelIndex: evt.target.value,
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <select>
-          {this.getMakeOptions()}
-        </select>
+        <div>
+          <select onChange={this.handleMakeSelect.bind(this)}>
+            <option value="">Make</option>
+            {this.getMakeOptions()}
+          </select>
 
-        <select>
-          {this.getModelOptions()}
-        </select>
+          <select onChange={this.handleModelSelect.bind(this)}
+            disabled={!this.state.makeSelected}>
+            <option value="">Model</option>
+            {this.getModelOptions()}
+          </select>
 
-        <select>
-          {this.getYearOptions()}
-        </select>
+          <select onChange={this.handleYearSelect.bind(this)}
+            disabled={!this.state.makeSelected || !this.state.modelSelected}>
+            <option value="">Year</option>
+            {this.getYearOptions()}
+          </select>
+        </div>
+
+        <div>
+          You Selected
+          a {this.state.makeSelected} {this.state.modelSelected} from
+          the year {this.state.yearSelected} that
+          has an index: {this.state.modelIndex}.
+
+        </div>
       </div>
     );
   }
